872892cc63183c2f0667f7a998f97ab5b3a012a4
Gianluca Gabrielli <crazybyte+github@protonmail.com>
2020-03-25 16:20:58 +0100

    Reverse history listing

--- zsh/plugins/fzf/key-bindings-custom.zsh
+++ zsh/plugins/fzf/key-bindings-custom.zsh
@@ -10,7 +10,7 @@ fzf-custom-history-widget() {
 local selected num
 setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
 selected=( $(history -Df 1 |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
+    FZF_DEFAULT_OPTS="--tac --height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
 local ret=$?
 if [ -n "$selected" ]; then
     num=$selected[1]
