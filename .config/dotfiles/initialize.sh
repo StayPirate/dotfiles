90a26a625e959e4102ed9b7952fa349d10fc51c9
Gianluca Gabrielli <gianluca.gabrielli@suse.com>
2021-03-01 19:25:07 +0100

    Initialize dotfiles-private and dotfiles-root in case are enabled

--- .config/dotfiles/initialize.sh
+++ .config/dotfiles/initialize.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# Create a lock file to show the first run has been executed
+# Create a lock file to mark this script as executed once
 touch $HOME/.config/dotfiles/first_run
 
 dotfiles() { git --git-dir="${HOME}/.config/dotfiles/public" --work-tree="${HOME}" "${@}"; }
+dotfiles-pvt() { git --git-dir="${HOME}/.config/dotfiles/private/.git" --work-tree="${HOME}" "${@}"; }
+dotfiles-root() { sudo git --git-dir="${HOME}/.config/dotfiles/root/.git" --work-tree=/ "${@}"; }
 export -f dotfiles
-
-# A HTTPS pull url is useful to fetch new commits without authentication (it's a public repo),
-# while a SSH push url is useful to push new commits via authentication
-dotfiles remote set-url --push origin git@github.com:StayPirate/dotfiles.git
-
-# Stop dotfiles to show untracked files in $HOME
-dotfiles config --local status.showUntrackedFiles no
+export -f dotfiles-pvt
+export -f dotfiles-root
 
 # Enabling pyenv-virtualenv plugin in pyenv
 if [[ -d "${HOME}/.config/pyenv/pyenv/plugins" && ! -L "${HOME}/.config/pyenv/pyenv/plugins/pyenv-virtualenv" ]]; then
     ln -s "${HOME}/.config/pyenv/pyenv-virtualenv" "${HOME}/.config/pyenv/pyenv/plugins"
 fi
 
-# Check disabled submodules
+# List disabled submodules
 IFS=$'\n'
-_disabled_submodules=($(dotfiles config -f .gitmodules --get-regexp update | grep none | awk '{print $1}' | cut -d '.' -f2))
+_disabled_submodules=($(dotfiles config --local --get-regexp update | grep none | awk '{print $1}' | cut -d '.' -f2))
 unset IFS
 
 [ ${#_disabled_submodules[@]} -eq 0 ] || echo "The following submodules are disabled:"
@@ -28,7 +25,6 @@ for _disabled_submodule in "${_disabled_submodules[@]}"; do
     read -p "${_disabled_submodule}, do you want to enable it? [y/N] " -n 1 -r 2>&1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        # Qui non funziona, in quanto anche cambiando to checkout, su 
         dotfiles config --local submodule.$_disabled_submodule.update checkout
         dotfiles submodule update --quiet --init --recursive >/dev/null 2>&1
         if [[ $? -ne 0 ]]; then
@@ -38,6 +34,21 @@ for _disabled_submodule in "${_disabled_submodules[@]}"; do
     fi
 done
 
+# A HTTPS pull url is useful to fetch new commits without authentication (it's a public repo),
+# while a SSH push url is handy to push new commits via Github keyfile authentication
+dotfiles remote set-url --push origin git@github.com:StayPirate/dotfiles.git > /dev/stderr
+
+# Stop dotfiles to show untracked files
+dotfiles config --local status.showUntrackedFiles no > /dev/stderr
+# If dotfiles-pvt doesn't return an error, it means it has been successfully enabled previously
+# then the user can proceed with the checkout of the files. Use -f to overwrite existing files
+dotfiles-pvt config --local status.showUntrackedFiles no > /dev/stderr
+[[ $? -eq 0 ]] && dotfiles-pvt checkout -f > /dev/stderr
+# If dotfiles-root doesn't return an error, it means it has been successfully enabled previously
+# then the user can proceed with the checkout of the files. Use -f to overwrite existing files
+dotfiles-root config --local status.showUntrackedFiles no > /dev/stderr
+[[ $? -eq 0 ]] && dotfiles-root checkout -f > /dev/stderr
+
 # Remove .zcompdump in the home, new cache location is "${_zsh_cache_dir}/zcompdump"
 [[ -f $HOME/.zcompdump ]] && rm $HOME/.zcompdump
 
@@ -46,7 +57,7 @@ systemctl --user daemon-reload
 for unit in `find $HOME/.config/systemd/user -maxdepth 1 -type f -printf "%f\n"`; do
     systemctl --user is-enabled --quiet $unit
     if [[ $? -ne 0 ]]; then
-        read -p "[systemd] do you want enable ${unit}? [y/N] " -n 1 -r 2>&1
+        read -p "[systemd] do you want enable and start ${unit}? [y/N] " -n 1 -r 2>&1
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             systemctl --user enable --quiet $unit
