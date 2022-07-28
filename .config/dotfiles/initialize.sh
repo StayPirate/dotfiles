f911212f0320caa9175e6d876e46400d58c8a5bf
Gianluca Gabrielli <gianluca.gabrielli@suse.com>
2022-07-28 16:30:22 +0200

    Remove the dotfiles-{root,private} submodules
    Until now I used dotfiles-root and dotfiles-private as dotfiles
    submodules. Unfortunately, there is no way (or at least I'm not aware
    of) to set bare submodules. Submodules get a main worktree on their
    initialization.
    I prefer to manage dotfiles-root and dotfiles-private as bare
    repositories and manage them the same way I do for dotfiles.
    
    Added short aliases: d, dp, dr.
    
    The initialization script has been updated with the new design choice,
    but BE CAREFUL it's not tested and for dr and dp any conflicting file
    will be overwritten !!!
    TODO: The whole script requires to be rethought, but I have no time
    right now.

--- .config/dotfiles/initialize.sh
+++ .config/dotfiles/initialize.sh
@@ -3,18 +3,32 @@
 # Create a lock file to mark this script as executed once
 touch $HOME/.config/dotfiles/first_run
 
+# Dotfiles repositories addresses
+DOTFILES_HTTPS=https://github.com/StayPirate/dotfiles.git
+DOTFILES_SSH=github.com:StayPirate/dotfiles.git
+
+DOTFILES_PRIVATE_HTTPS=https://github.com/StayPirate/dotfiles-private.git
+DOTFILES_PRIVATE_SSH=git@github.com:StayPirate/dotfiles-private.git
+
+DOFTILES_ROOT_HTTPS=https://github.com/StayPirate/dotfiles-root.git
+DOFTILES_ROOT_SSH=git@github.com:StayPirate/dotfiles-root.git
+
 dotfiles() { git --git-dir="${HOME}/.config/dotfiles/public" --work-tree="${HOME}" "${@}"; }
-dotfiles-private() { git --git-dir="${HOME}/.config/dotfiles/private/.git" --work-tree="${HOME}" "${@}"; }
-dotfiles-root() { sudo git --git-dir="${HOME}/.config/dotfiles/root/.git" --work-tree=/ "${@}"; }
+dotfiles-private() { git --git-dir="${HOME}/.config/dotfiles/private" --work-tree="${HOME}" "${@}"; }
+dotfiles-root() { sudo git --git-dir="${HOME}/.config/dotfiles/root" --work-tree=/ "${@}"; }
+
 export -f dotfiles
 export -f dotfiles-private
 export -f dotfiles-root
 
 # Enabling pyenv-virtualenv plugin in pyenv
+# it needs to be done here as the `${HOME}/.config/pyenv/pyenv/plugins` folder is created only after the pyenv submodule
+# was initialized. Hence, I cannot simply add the symbolic link to my dotfiles.
 if [[ -d "${HOME}/.config/pyenv/pyenv/plugins" && ! -L "${HOME}/.config/pyenv/pyenv/plugins/pyenv-virtualenv" ]]; then
     ln -s "${HOME}/.config/pyenv/pyenv-virtualenv" "${HOME}/.config/pyenv/pyenv/plugins"
 fi
 
+# Ask the user which submodules enable (if there are any disabled)
 # List disabled submodules
 IFS=$'\n'
 _disabled_submodules=($(dotfiles config --local --get-regexp update | grep none | awk '{print $1}' | cut -d '.' -f2))
@@ -34,25 +48,55 @@ for _disabled_submodule in "${_disabled_submodules[@]}"; do
     fi
 done
 
-# A HTTPS pull url is useful to fetch new commits without authentication (it's a public repo),
-# while a SSH push url is handy to push new commits via Github keyfile authentication
+# Fetch git commits via HTTPS is handy as you can pull them without authentication (if public repo),
+# while push commits via SSH is good as you are automatically authenticated.
+# For this reason I set the remote pull url with HTTPS and push url with GIT
+# Example: > dot remote -v
+#          origin  https://github.com/StayPirate/dotfiles.git (fetch)
+#          origin  git@github.com:StayPirate/dotfiles.git (push)
 dotfiles remote set-url --push origin git@github.com:StayPirate/dotfiles.git > /dev/stderr
 
-# Stop dotfiles to show untracked files
+# This is a very important step.
+# As we use $HOME as worktree, all the contained files will be listed as `untracked` when you type
+# something like `dot status`, making the dotfiles management via git almost impossible.
+# Setting status.showUntrackedFiles to no will stop this behavior in git, and only the manually added
+# files will be shown in git output. So, you just need to use `git add file_x` to make file_x versionated
+# in git.
 dotfiles config --local status.showUntrackedFiles no > /dev/stderr
-# If dotfiles-private doesn't return an error, it means it has been successfully enabled previously
-# then the user can proceed with the checkout of the files. Use -f to overwrite existing files
-dotfiles-private config --local status.showUntrackedFiles no > /dev/stderr
-[[ $? -eq 0 ]] && dotfiles-private checkout -f > /dev/stderr
-# If dotfiles-root doesn't return an error, it means it has been successfully enabled previously
-# then the user can proceed with the checkout of the files. Use -f to overwrite existing files
-dotfiles-root config --local status.showUntrackedFiles no > /dev/stderr
-[[ $? -eq 0 ]] && dotfiles-root checkout -f > /dev/stderr
+
+# Lets try to clone dotfiles-private and dotfiles-root as bare repositories as well.
+# These are private repositories, so it will fail if the user is not authorized to access them (ssh-key).
+
+# If git clone succeeded
+if git clone --bare $DOTFILES_PRIVATE_HTTPS ~/.config/dotfiles/private 2>&1; then
+    # If the bare repo si correctly instantiated
+    if [[ -f $HOME/.config/dotfiles/private/HEAD ]]; then
+        # Set remote push url via git
+        dotfiles-private remote set-url --push origin git@$DOTFILES_PRIVATE_SSH > /dev/stderr
+        # Do not shown untracked files
+        dotfiles-private config --local status.showUntrackedFiles no > /dev/stderr
+        # Copy all the files in the repository to $HOME. Please note that -f to overwrite existing files
+        dotfiles-private checkout -f
+    fi
+fi
+
+# If git clone succeeded
+if git clone --bare $DOTFILES_ROOT_HTTPS ~/.config/dotfiles/root 2>&1; then
+    # If the bare repo si correctly instantiated
+    if [[ -f $HOME/.config/dotfiles/root/HEAD ]]; then
+        # Set remote push url via git
+        dotfiles-root remote set-url --push origin git@$DOTFILES_ROOT_SSH > /dev/stderr
+        # Do not shown untracked files
+        dotfiles-root config --local status.showUntrackedFiles no > /dev/stderr
+        # Copy all the files in the repository to $HOME. Please note that -f to overwrite existing files
+        dotfiles-root checkout -f
+    fi
+fi
 
 # Remove .zcompdump in the home, new cache location is "${XDG_CACHE_HOME:-$HOME/.cache}/zcompdump"
 [[ -f $HOME/.zcompdump ]] && rm $HOME/.zcompdump
 
-# Check disabled custom services
+# Check disabled custom systemd services and ask the user if want to start and enable them
 systemctl --user daemon-reload
 for unit in `find $HOME/.config/systemd/user -maxdepth 1 -type f -printf "%f\n"`; do
     systemctl --user is-enabled --quiet $unit
--- .config/dotfiles/install.sh
+++ .config/dotfiles/install.sh
@@ -21,41 +21,58 @@ echo "                [https://github.com/StayPirate/dotfiles]              "
 echo -e "\n\n"
 dotfiles() { git --git-dir="${HOME}/.config/dotfiles/public" --work-tree="${HOME}" "${@}"; }
 export -f dotfiles
-[ -z $XDG_RUNTIME_DIR ] && XDG_RUNTIME_DIR=/tmp
-_log_file=$(mktemp -t dotfile-XXXX.log -p $XDG_RUNTIME_DIR)
-echo -e "${_orange}[*]${_no_color} Instalaltion logs at ${_log_file}."
 
+# Check if git is installed
 type git >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then echo -e "\t${_red}[!]${_no_color} Abort: git is not installed." && exit 1; fi
 
+# Abort installation if the dotfiles repository already exists in the system
 if [[ -f $HOME/.config/dotfiles/public/HEAD ]]; then
     echo -e "${_red}[!]${_no_color} Abort: a dotfiles repository exists in ${HOME}/.config/dotfiles/public."
     exit 1
 fi
 
+# Create the installation log file
+_log_file=$(mktemp -t dotfile-XXXX.log -p ${XDG_RUNTIME_DIR:-/tmp})
+echo -e "${_orange}[*]${_no_color} Instalaltion logs at ${_log_file}."
+
+# Clone the dotfiles bare repository
 echo "[.] Cloning dotfiles repository..."
 git clone --bare https://github.com/StayPirate/dotfiles.git ~/.config/dotfiles/public > "$_log_file" 2>&1
+
+# Abort if git failed to clone the repo
 if [[ $? -ne 0 ]]; then echo -e "${_red}[!]${_no_color} Abort: cannot clone the dotfiles repository." && exit 1; fi
+
+# Populate the worktree
 echo "[.] Branch checkout to ${HOME}"
 cd $HOME && dotfiles checkout > "$_log_file" 2>&1
+
+# It fails if files already exists in $HOME, in that case let's move all the involved files in a safe place
+# (a backup folder) and try to populate again the $HOME
 if [[ $? -ne 0 ]]; then
     IFS=$'\n'
-    # Get the list of files that cannot be checked out because already exist in the worktree (in this case $HOME)
+    # Get the list of files that cannot be checked out because already exist in the worktree
     _existing_files=($(dotfiles checkout 2>&1 | grep -E "^[[:space:]]+" | sed 's/^\s*//' | sort -u))
     unset IFS
+    # Create an unique backup folder
     _now=$(date +"%Y%m%d_%H%M%S")
     echo "[.] Backing up the following file in ${HOME}/.config/dotfiles/backup-${_now}:"
+    # Move blocking files to the backup folder while keeping the same directory-tree
     for _existing_file in "${_existing_files[@]}"; do
         echo -e "\t${_existing_file}"
         _file_dir="$(dirname "$_existing_file")"
         mkdir -p "${HOME}/.config/dotfiles/backup-${_now}/${_file_dir}"
         mv "${_existing_file}" "${HOME}/.config/dotfiles/backup-${_now}/${_existing_file}" > "$_log_file" 2>&1
     done
+    # Try again to populate the worktree, this time should work as no existing file with same name should exist in $HOME
     dotfiles checkout > "$_log_file" 2>&1
+    # If keep failing there should be another problem, so abort the installation
     if [[ $? -ne 0 ]]; then
         echo -e "${_orange}[*]${_no_color} Something went wrong during the branch checkout, please check the logs."
     fi
 fi
+
+# Initialize submodules
 echo "[.] Initializing git submodules"
 dotfiles submodule update --init --recursive > "$_log_file" 2>&1
 
@@ -69,7 +86,7 @@ else
     grep "$USER.*zsh.*" /etc/passwd >/dev/null 2>&1 && _zsh_is_not_default=false
     if [[ "$_zsh_is_not_default" = true ]]; then
         echo -e "${_orange}[*]${_no_color} Zsh is not the default shell for ${USER}"
-        # Check if $USER can sudo and try to set zsh as default shell
+        # If the $USER can sudo, try to set zsh as default shell
         sudo -l >/dev/null 2>&1
         if [[ $? -eq 0 ]]; then
             echo "[.] Executing: sudo chsh -s $(which zsh) ${USER}"
@@ -84,4 +101,5 @@ else
     fi
 fi
 
+# If you are here the installation succeeded
 echo -e "${_green}[*]${_no_color} Installation done"
